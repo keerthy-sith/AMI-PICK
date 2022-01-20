b50861ba5ea56d08787e743ebb8957194b986904
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-20 12:54:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "My-ec2" {
-        instance_type="${var.instance_type == "windows" ? 1: 0 }
-        ami="${var.ec2_ami == "windows" ? 1: 0 }
+        instance_type="$lookup{var.instance_type,var.env_name)}"
+        ami="$lookup{var.ec2_ami,var.env_name)}"
         tags ={
                 Name="${var.env_name == "windows" ? 1: 0 }
         }
