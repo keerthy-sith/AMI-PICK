d6c3a3627ae73ce7256b6cb494d5b7a25320d48e
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-20 13:21:33 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,23 +1,8 @@
 variable "instance_type" {
-	type=map
-	default={
-		Linux="linux.tfvars"
-		Windows="windows.tfvars"
-	}
 }
 variable "ec2_ami" {
-  type=map
-	default={
-		Linux="linux.tfvars"
-		Windows="windows.tfvars"
-	}
 }
 variable "env_name"{
-	type=map
-	default={
-		Linux="linux.tfvars"
-		Windows="windows.tfvars"
-	}
 }
 
 
